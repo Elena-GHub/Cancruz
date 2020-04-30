c25f8a2723b3c97eda2f162ea771abb4d483e7da
Elena <cabjelen@yahoo.es>
2020-04-30 07:27:37 +0200

    "Elena/Five fake users seeding to DB added"

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\User::class)->create(['first_name' => 'Deyvid', 'last_name' => 'Cruz', 'email' => 'deyvid.cruz@cancruz.com']);
+        factory(\App\User::class)->create(['first_name' => 'Deyvid', 'last_name' => 'Cruz', 'email' => 'deyvid.cruz@cancruz.com', 'admin' =>'1' ]);
         factory(\App\Room::class)->create(['room_name' => 'Suite PUIGRACIÓS']);
         factory(\App\Room::class)->create(['room_name' => 'Suite SANT CRISTÒFOL']);
         factory(\App\Room::class)->create(['room_name' => 'Suite ROCACENTELLA']);
+        factory(\App\User::class, 5)->create();
     }
 }
