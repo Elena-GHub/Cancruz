883c9be0d72916453049facf9bce88be5b92a1c9
Javier Mármol <javiermarmolperez@gmail.com>
2020-05-04 12:40:50 +0200

    Javier/ cambios en bootstrap

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\User::class)->create(['first_name' => 'Deyvid', 'last_name' => 'Cruz', 'email' => 'deyvid.cruz@cancruz.com', 'admin' =>'1' ]);
+        factory(\App\User::class)->create(['first_name' => 'Deyvid', 'last_name' => 'Cruz', 'email' => 'deyvid.cruz@cancruz.com']);
         factory(\App\Room::class)->create(['room_name' => 'Suite PUIGRACIÓS']);
         factory(\App\Room::class)->create(['room_name' => 'Suite SANT CRISTÒFOL']);
         factory(\App\Room::class)->create(['room_name' => 'Suite ROCACENTELLA']);
-        factory(\App\User::class, 5)->create();
     }
 }
