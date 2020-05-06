b4fb9db7723a4f7e3fefe13ef1cb537c42b23772
Elena <cabjelen@yahoo.es>
2020-05-06 17:40:20 +0200

    Elena/changed Deyvid's password

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\User::class)->create(['first_name' => 'Deyvid', 'last_name' => 'Cruz', 'email' => 'deyvid.cruz@cancruz.com', 'admin' =>'1' ]);
+        factory(\App\User::class)->create(['first_name' => 'Deyvid', 'last_name' => 'Cruz', 'email' => 'deyvid.cruz@cancruz.com', 'admin' =>'1', 'password' => 'patata']);
         factory(\App\Room::class)->create(['room_name' => 'Suite PUIGRACIÓS']);
         factory(\App\Room::class)->create(['room_name' => 'Suite SANT CRISTÒFOL']);
         factory(\App\Room::class)->create(['room_name' => 'Suite ROCACENTELLA']);
