9df148ba2832c6d9fa788bdeb3811fb4825e6dce
Elena <cabjelen@yahoo.es>
2020-05-20 15:54:21 +0200

    Elena/fixes copy-paste mistakes

--- resources/views/client/edit.blade.php
+++ resources/views/client/edit.blade.php
@@ -35,39 +35,40 @@
 <div>
     <div class="container">
         <div class="card">
-            <form action="{{Route('client.store')}}" method="POST">
+            <form action="{{Route('client.update',$client->id)}}" method="POST">
                 @csrf
+                @method('put')
                     <div class="card-header col-12 bg-primary my-card">
-                        <strong>Rellene todos los campos</strong>
+                        <strong>Actualice los campos pertinentes</strong>
                     </div>
                     <div class="card-columns">
                         <div class="card-body form-group">
                             <div class="card my-card bg-secondary">
                                 <label>Nombre:</label>
-                                <input type="text" name="first_name" class="form-control" />
+                                <input type="text" name="first_name" class="form-control" value="{{$client->first_name}}" />
                                 <label class="my-secondItem">Apellido(s):</label>
-                                <input type="text" name="last_name" class="form-control" />
+                                <input type="text" name="last_name" class="form-control" value="{{$client->last_name}}" />
                             </div>
                         </div>
                         <div class="card-body form-group">
                             <div class="card my-card bg-secondary">
                                 <label>Correo electrónico:</label>
-                                <input type="text" name="email" class="form-control" />
+                                <input type="text" name="email" class="form-control" value="{{$client->email}}" />
                                 <label class="my-secondItem">Teléfono:</label>
-                                <input type="text" name="phone_number" class="form-control" />
+                                <input type="text" name="phone_number" class="form-control" value="{{$client->phone_number}}" />
                             </div>
                         </div>
                         <div class="card-body form-group">
                             <div class="card my-card bg-secondary">
                                 <label>Habitación:</label>
                                     <select name="room_id" class="form-control" />
-                                    <optgroup label="Seleccione una habitación">
+                                    <optgroup label="actual: {{$client->room->room_name}}">
                                         @foreach ($rooms as $room)
                                         <option value="{{ $room['id'] }}">{{ $room['room_name'] }}</option>
                                         @endforeach
                                     </select>
                                 <label class="my-secondItem">Nº de huéspedes:</label>
-                                <input type="text" name="guests" class="form-control" />
+                                <input type="text" name="guests" class="form-control" value="{{$client->guests}}" />
                             </div>
                             </div>
                         </div>
@@ -94,23 +95,23 @@
                                 <div class="card my-card bg-secondary">
                                     <label>Extras:</label><br>
                                     <input type="hidden" name="pet" value="0" />
-                                    <input id="pet" class="w3-radio" type="checkbox" name="pet" value="1">
+                                    <input type="checkbox" name="pet" value="1" <?php if($client->pet == "Sí") echo "checked"; ?>/>
                                     <label class="w3-validate">Mascota</label><br>
                                     <input type="hidden" name="breakfast" value="0" />
-                                    <input id="breakfast" class="w3-radio" type="checkbox" name="breakfast" value="1">
+                                    <input type="checkbox" name="breakfast" value="1" <?php if($client->breakfast == "Sí") echo "checked"; ?>/>
                                     <label class="w3-validate">Desayuno</label>
                                 </div>
                             </div>
                             <div class="card-body form-group">
                                 <div class="card my-card bg-secondary">
                                     <label>Comentarios:</label>
-                                <input type="text" name="comments" class="form-control" />
+                                <input type="text" name="comments" class="form-control" value="{{$client->comments}}" />
                                 </div>
-                            </div>                   
-                              
+                            </div>
+                        
                     </div>
                     <div class="card-footer">
-                        <input type="submit" value="Crear" class="btn btn-primary">
+                        <input type="submit" value="Actualizar" class="btn btn-primary">
                     </div>
                 </div>
             </form>
