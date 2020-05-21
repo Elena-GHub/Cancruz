d47b24151216fbe0b6d9d4ee3ad09df60535516b
ClaudiaCasals <59821769+ClaudiaCasals@users.noreply.github.com>
2020-05-21 17:57:12 +0200

    claudia/creacion front abaout con rutas

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,87 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    .cabezera{
+        margin-top : -26%;
+    }
+</style>
 
+<div class="banner agileits w3layouts">
+    <img src="images\Garden hands.png" alt="Agileits W3layouts" class="cabezera">
+    <h1 class="wow agileits w3layouts fadeInDown">Sobre nosotros</h1>
+</div>
+<!-- //Banner -->
+<div class="team agileits w3layouts">
+    <div class="container">
+
+        <h3>Nuestro equipo</h3>
+
+        <div class="team-grids agileits w3layouts grid">
+            <div class="col-md-4 col-sm-4 team-grid agileits w3layouts team-grid1 wow slideInUp">
+                <figure class="effect-zoe agileits w3layouts">
+                    <img src="images\Nosotros\6aeb3a82-bdb5-4938-920b-655860e0ec5b.jpg" alt="Agileits W3layouts"/>
+                    <figcaption>
+                        <h4>Javier<span>Marmol</span></h4>
+
+                        <p class="description agileits w3layouts">El agricultor</p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-md-4 col-sm-4 team-grid agileits w3layouts team-grid2 wow slideInUp">
+                <figure class="effect-zoe agileits w3layouts">
+                    <img src="images\Nosotros\7bbfe57e-776b-46e5-8f5a-5b47f3ab5f9e.jpg" alt="Agileits W3layouts"/>
+                    <figcaption>
+                        <h4>Diego <span>Cordoba</span></h4>
+
+                        <p class="description agileits w3layouts">El cocinero</p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-md-4 col-sm-4 team-grid agileits w3layouts team-grid3 wow slideInUp">
+                <figure class="effect-zoe agileits w3layouts">
+                    <img src="images\Nosotros\8b8838e6-ad1b-49e2-aa7a-d4ba9a64d15f.jpg" alt="Agileits W3layouts"/>
+                    <figcaption>
+                        <h4>Elena <span>Carballido</span></h4>
+
+                        <p class="description agileits w3layouts">La recepcionista</p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-md-4 col-sm-4 team-grid agileits w3layouts team-grid2 wow slideInUp">
+                <figure class="effect-zoe agileits w3layouts">
+                    <img src="images\Nosotros\60c14d92-4caa-49c9-a788-eec29b35e25e.jpg" alt="Agileits W3layouts"/>
+                    <figcaption>
+                        <h4>Magali<span>Briseida</span></h4>
+
+                        <p class="description agileits w3layouts">La granjera</p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-md-4 col-sm-4 team-grid agileits w3layouts team-grid2 wow slideInUp">
+                <figure class="effect-zoe agileits w3layouts">
+                    <img src="images\Nosotros\a23b9c0f-7f6f-44cd-b9c8-36b3c852b5e1.jpg" alt="Agileits W3layouts"/>
+                    <figcaption>
+                        <h4>Claudia<span>Casals</span></h4>
+
+                        <p class="description agileits w3layouts">La organizadora</p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-md-4 col-sm-4 team-grid agileits w3layouts team-grid2 wow slideInUp">
+                <figure class="effect-zoe agileits w3layouts">
+                    <img src="images\Nosotros\pp.jpg" alt="Agileits W3layouts"/>
+                    <figcaption>
+                        <h4>THE FINAL<span>BOSSES</span></h4>
+
+                        <p class="description agileits w3layouts">Los responsables</p>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+
+    </div>
+</div>
 
 @endsection
